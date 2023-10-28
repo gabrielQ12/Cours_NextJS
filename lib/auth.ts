f3b535d01e41c74e290bd5e3ce21dd90805972f7
gabrielQ12 <gabriel.quinette1@gmail.com>
2023-10-28 14:52:13 +0200

    setup_prisma_DONE

---
+++ lib/auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import { env } from "./env";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_ID,
+      clientSecret: env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+}
+
+export default NextAuth(authOptions)
